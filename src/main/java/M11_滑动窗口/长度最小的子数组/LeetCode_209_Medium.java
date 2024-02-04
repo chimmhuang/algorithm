09496a05bdcc0f85e50f874fd91ef6bdf7c3562b
吃茫茫 <huangshuai@sinoiov.com>
2024-02-04 21:40:29 +0800

    feat: 新增长度最小的子数组

--- src/main/java/M11_滑动窗口/长度最小的子数组/LeetCode_209_Medium.java
+++ src/main/java/M11_滑动窗口/长度最小的子数组/LeetCode_209_Medium.java
@@ -14,39 +14,39 @@ public class LeetCode_209_Medium {
 
                  L/R
                   ↓
-        第1次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  sum=2，小于target， R右移
+        第1次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  窗口值=2，小于target， R右移，窗口值+=R值
 
                   L   R
                   ↓   ↓
-        第2次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  sum=5，小于target， R右移
+        第2次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  窗口值=5，小于target， R右移，窗口值+=R值
 
                   L       R
                   ↓       ↓
-        第3次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  sum=6，小于target， R右移
+        第3次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  窗口值=6，小于target， R右移，窗口值+=R值
 
                   L           R
                   ↓           ↓
-        第4次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  sum=8，大于target， L右移
+        第4次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  窗口值=8，大于target， L右移，窗口值-=L值
 
                       L       R
                       ↓       ↓
-        第5次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  sum=6，小于target， R右移
+        第5次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  窗口值=6，小于target， R右移，窗口值+=R值
 
                       L           R
                       ↓           ↓
-        第6次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  sum=10，大于target， L右移
+        第6次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  窗口值=10，大于target，L右移，窗口值-=L值
 
                           L       R
                           ↓       ↓
-        第7次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  sum=7，等于target， LR都右移 length=3
+        第7次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  窗口值=7，等于target， LR都右移 窗口值=窗口值-L值+R值 length=3
 
                               L       R
                               ↓       ↓
-        第8次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  sum=9，大于target， L右移
+        第8次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  窗口值=9，大于target， L右移，窗口值-=L值
 
                                   L   R
                                   ↓   ↓
-        第9次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  sum=7，等于target， R无法右移，结束滑动 length=2
+        第9次滑: [ 2 , 3 , 1 , 2 , 4 , 3 ]  窗口值=7，等于target， R无法右移，结束滑动 length=2
      */
     class Solution {
         public int minSubArrayLen(int target, int[] nums) {
@@ -55,19 +55,23 @@ public class LeetCode_209_Medium {
             int length = nums.length;
 
             int result = 0;
+            int sum = nums[0];
             while (rightIndex < length) {
-                int sum = 0;
-                for (int i = leftIndex; i <= rightIndex; i++) {
-                    sum += nums[i];
-                }
                 if (sum == target) {
-                    leftIndex++;
-                    rightIndex++;
                     // 计算长度，按照题目要求取最小的长度。
                     int elementLength = rightIndex - leftIndex + 1;
                     result = result == 0 ? elementLength : Math.min(result, elementLength);
-                } else if (sum < target) {
+
+                    leftIndex++;
                     rightIndex++;
+
+                    if (rightIndex < nums.length) {
+                        sum = sum - nums[(leftIndex - 1)] + nums[rightIndex];
+                    }
+                } else if (sum < target) {
+                    if (++rightIndex < nums.length) {
+                        sum += nums[rightIndex];
+                    }
                 } else {
                     // 计算长度，按照题目要求取最小的长度。
                     int elementLength = rightIndex - leftIndex + 1;
@@ -75,6 +79,11 @@ public class LeetCode_209_Medium {
                     leftIndex++;
                     if (leftIndex > rightIndex) {
                         rightIndex++;
+                        if (rightIndex < nums.length) {
+                            sum = sum - nums[(leftIndex - 1)] + nums[rightIndex];
+                        }
+                    } else {
+                        sum -= nums[leftIndex - 1];
                     }
                 }
             }
